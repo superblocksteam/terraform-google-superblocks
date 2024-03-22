64dd29db9670b03affed126fb993c8ddeba9ae84
Bruce Yu <51087152+bruce-y@users.noreply.github.com>
2024-03-22 10:42:52 -0400

    Set read only root (#17)

--- main.tf
+++ main.tf
@@ -44,6 +44,7 @@ module "cloud_run" {
     "SUPERBLOCKS_ORCHESTRATOR_AGENT_TAGS"              = "${var.superblocks_agent_tags}"
     "SUPERBLOCKS_ORCHESTRATOR_DATA_DOMAIN"             = "${var.superblocks_agent_data_domain}"
     "SUPERBLOCKS_ORCHESTRATOR_HANDLE_CORS"             = "${var.superblocks_agent_handle_cors}"
+    "S6_READ_ONLY_ROOT"                                = "1"
   }, var.superblocks_additional_env_vars)
   container_cpu_throttling  = var.container_cpu_throttling
   container_requests_cpu    = var.container_requests_cpu
